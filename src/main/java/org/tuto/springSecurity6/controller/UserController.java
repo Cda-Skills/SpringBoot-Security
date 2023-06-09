8a73d1713e0aa6fc132c59c99d10c74b6cd3bff6
Cyril <advert.doalto@gmail.com>
2023-06-09 17:27:36 +0200

    la création d'utilisateur dans la BDD H2 fonctionne (sauf la date en champ "hidden")

---
+++ src/main/java/org/tuto/springSecurity6/controller/UserController.java
@@ -0,0 +1,38 @@
+package org.tuto.springSecurity6.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.tuto.springSecurity6.model.User;
+import org.tuto.springSecurity6.service.UserService;
+
+@Controller
+public class UserController {
+
+	@Autowired
+	UserService userService;
+	
+	@RequestMapping("/inscription")
+		public String formulaire() {
+		
+		return "formulaire";
+	}
+	
+	@RequestMapping("/save")
+	public String Inscrit(@ModelAttribute("userAddtribute") User user){
+		userService.saveUser(user);
+		return "formulaire";
+	}
+	
+	@RequestMapping("/listUser")
+	public String listUser(ModelMap modelMap) {
+		List<User> userController = userService.getAllUsers();
+		modelMap.addAttribute("user", userController);
+		return "listUser";
+		
+	}
+}
